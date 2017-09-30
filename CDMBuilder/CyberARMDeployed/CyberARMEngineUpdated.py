2be65d327fb5da711628d778744bba7092b419c9
Dutta <adutta6@uncc.edu>
2017-09-30 17:03:47 -0400

    CCAA Meeting Week

--- CDMBuilder/CyberARMDeployed/CyberARMEngineUpdated.py
+++ CDMBuilder/CyberARMDeployed/CyberARMEngineUpdated.py
@@ -3,6 +3,18 @@ import ThreatPrioritization,ProjectConfigFile,ThreatPrioritizationExperience
 from z3 import *
 import time
 
+
+def make_comparator(less_than):
+    def compare(x, y):
+        if less_than(x, y):
+            return -1
+        elif less_than(y, x):
+            return 1
+        else:
+            return 0
+    return compare
+
+
 def generate_risk_distribution(asset_enterprise_list):
     global_risk_threat_action = []
     risk_threat_action_distribution = [[] for i in range(2)]
@@ -15,9 +27,27 @@ def generate_risk_distribution(asset_enterprise_list):
     ThreatPrioritizationExperience.threat_prioritization_main(prob_threat_experience,prob_threat_threat_action_experience,prob_threat_action_threat_experience,risk_threat_action_distribution[ProjectConfigFile.EXPERIENCE_LIST],risk_threat_distribution[ProjectConfigFile.EXPERIENCE_LIST],asset_enterprise_list[ProjectConfigFile.EXPERIENCE_LIST])
     for i in range(len(risk_threat_action_distribution)):
         for j in range(len(risk_threat_action_distribution[i])):
-            print "Risk %s : %s" % (i,risk_threat_action_distribution[i][j])
+            print "Risk (%s,%s) : %s" % (i,j,risk_threat_action_distribution[i][j])
             for ta in risk_threat_action_distribution[i][j].keys():
-                global_risk_threat_action.append(risk_threat_action_distribution[i][j][ta])
+                global_risk_threat_action.append([risk_threat_action_distribution[i][j][ta],i,j])
     global_risk_threat_action.sort(reverse=True)
     print "Risk Threat Action %s" % (global_risk_threat_action)
-    print "Global Risk %s Total length %s" % (sum(global_risk_threat_action),len(global_risk_threat_action))
+    risk_length = len(global_risk_threat_action)
+    print "Length %s" % (risk_length)
+    total_risk_value = sum([global_risk_threat_action[i][0] for i in range(risk_length)])
+    print "Total Risk %s" % (total_risk_value)
+    sum_percentage = []
+    start_index = 0
+    distance_percentage = risk_length/10
+    init_sum = 0
+    while start_index < risk_length:
+        last_index = start_index + distance_percentage
+        if last_index > risk_length:
+            last_index = risk_length
+        print "Start Index %s : End Index %s" % (start_index,last_index)
+        while start_index < last_index:
+            init_sum += global_risk_threat_action[start_index][0]
+            start_index += 1
+        percentage_sum = round(init_sum/float(total_risk_value)*100,3)
+        sum_percentage.append([percentage_sum,round(start_index/float(risk_length)*100,3)])
+    print "This is the percentage %s" % (sum_percentage)
--- static/utilities_Custom.js
+++ static/utilities_Custom.js
@@ -355,4 +355,4 @@ function loadDataGrid(dataLoad){
     }
 
 
-    
+
