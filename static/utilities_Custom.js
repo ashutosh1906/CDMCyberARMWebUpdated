1e3540e23a4dfc1ca6ab209705a8afeec9751d2e
Dutta <adutta6@uncc.edu>
2017-10-03 00:23:16 -0400

    Save Input Files

--- static/utilities_Custom.js
+++ static/utilities_Custom.js
@@ -170,8 +170,8 @@ function loadDataGridUpdated(dataLoad){
                 datatype: "array",
                 // sort: customsortfunc,
                 datafields: [{ name: 'sc_name'},
-                  { name: 'sc_version'},
-                  { name: 'sc_func',},
+                  { name: 'sc_version',type:'float'},
+                  { name: 'sc_func'},
                   { name: 'en_level_name',},
                   { name: 'kc_phase_name', },
                     { name: 'explanation_row', },
@@ -237,18 +237,52 @@ function loadDataGrid(dataLoad){
 
                 ]
             });
-    }
+}
 
-    function loadDataGridThreatAction(dataLoad){
+function loadDataGridThreatAction(dataLoad){
+        console.log(dataLoad);
+        var source =
+        {
+            localdata: dataLoad,
+            datatype: "array",
+            datafields: [{ name: 'sc_version'},
+              { name: 'security_control_name'},
+              { name: 'threat_action'},
+
+       ] };
+
+        var dataAdapter = new $.jqx.dataAdapter(source, {
+            loadComplete: function (data) { },
+            loadError: function (xhr, status, error) { }
+        });
+        $("#jqxgrid").jqxGrid(
+        {
+            source: dataAdapter,
+            autowidth: true,
+            autoheight:true,
+            pageable:true,
+            sortable: true,
+            filterable: true,
+// cellValue - cell's current value, row data - row's data, data field - column's data field, filterGroup - group of filters, defaultFilterResult - the built-in boolean evaluated result-
+
+            columns: [
+                {text: 'Security Control Version', datafield: 'sc_version', width:250,renderer:columnrenderer,cellsrenderer:cellsrenderer},
+              { text: 'Security Control', datafield: 'security_control_name', width:450,renderer:columnrenderer,cellsrenderer:cellsrenderer},
+              { text: 'Threat Action', datafield:'threat_action', width:370,renderer:columnrenderer,cellsrenderer:cellsrenderer},
+
+            ]
+        });
+}
+
+/////////////////////////////////////////////////// Threat Action To Kill Chain Phase Mapping ///////////////////////////////////////////////////////////////////
+function loadDataGridThreatActionKCPhase(dataLoad){
             console.log(dataLoad);
             var source =
             {
                 localdata: dataLoad,
                 datatype: "array",
-                datafields: [{ name: 'sc_version'},
-                  { name: 'security_control_name'},
-                  { name: 'threat_action'},
-
+                datafields: [{ name: 'threat_action'},
+                  { name: 'kc_phase'},
            ] };
 
             var dataAdapter = new $.jqx.dataAdapter(source, {
@@ -266,126 +300,124 @@ function loadDataGrid(dataLoad){
     // cellValue - cell's current value, row data - row's data, data field - column's data field, filterGroup - group of filters, defaultFilterResult - the built-in boolean evaluated result-
 
                 columns: [
-                    {text: 'Security Control Version', datafield: 'sc_version', width:250,renderer:columnrenderer,cellsrenderer:cellsrenderer},
-                  { text: 'Security Control', datafield: 'security_control_name', width:450,renderer:columnrenderer,cellsrenderer:cellsrenderer},
-                  { text: 'Threat Action', datafield:'threat_action', width:370,renderer:columnrenderer,cellsrenderer:cellsrenderer},
-
+                    {text: 'Threat Action', datafield: 'threat_action', width:350,renderer:columnrenderer,cellsrenderer:cellsrenderer},
+                    { text: 'Kill Chain Phase', datafield: 'kc_phase', width:350,renderer:columnrenderer,cellsrenderer:cellsrenderer},
                 ]
             });
     }
 
-    /////////////////////////////////////////////////// Threat Action To Kill Chain Phase Mapping ///////////////////////////////////////////////////////////////////
-    function loadDataGridThreatActionKCPhase(dataLoad){
-                console.log(dataLoad);
-                var source =
-                {
-                    localdata: dataLoad,
-                    datatype: "array",
-                    datafields: [{ name: 'threat_action'},
-                      { name: 'kc_phase'},
-               ] };
 
-                var dataAdapter = new $.jqx.dataAdapter(source, {
-                    loadComplete: function (data) { },
-                    loadError: function (xhr, status, error) { }
-                });
-                $("#jqxgrid").jqxGrid(
-                {
-                    source: dataAdapter,
-                    autowidth: true,
-                    autoheight:true,
-                    pageable:true,
-                    sortable: true,
-                    filterable: true,
-        // cellValue - cell's current value, row data - row's data, data field - column's data field, filterGroup - group of filters, defaultFilterResult - the built-in boolean evaluated result-
-
-                    columns: [
-                        {text: 'Threat Action', datafield: 'threat_action', width:350,renderer:columnrenderer,cellsrenderer:cellsrenderer},
-                        { text: 'Kill Chain Phase', datafield: 'kc_phase', width:350,renderer:columnrenderer,cellsrenderer:cellsrenderer},
-                    ]
-                });
-        }
-
-
-    function loadDataGridWithName(dataLoad,name){
-            console.log("(...)(..)(..)(..)"+dataLoad);
-            var source =
-            {
-                localdata: dataLoad,
-                datatype: "array",
-                datafields: [{ name: 'sc_control'},
-                  { name: 'id',type:'float'},
-                  { name: 'sec_func',},
-                  { name: 'en_level',},
-                  { name: 'kill_phase', },
-
-           ] };
-
-            var dataAdapter = new $.jqx.dataAdapter(source, {
-                loadComplete: function (data) { },
-                loadError: function (xhr, status, error) { }
-            });
-            $("#"+name).jqxGrid(
-            {
-                source: dataAdapter,
-                autowidth: true,
-                autoheight:true,
-                pageable:true,
-                sortable: true,
-                columns: [
-                  { text: 'Security Control', datafield: 'sc_control', width: 450,renderer:columnrenderer,cellsrenderer:cellsrenderer},
-                  { text: 'Id', datafield:'id', width:  70,renderer:columnrenderer,cellsrenderer:cellsrenderer},
-                  { text: 'Security Function', datafield: 'sec_func', width: 180,renderer:columnrenderer,cellsrenderer:cellsrenderer },
-                  { text: 'Enforcement Level', datafield: 'en_level', width: 180, cellsalign: 'right',renderer:columnrenderer,cellsrenderer:cellsrenderer },
-                  { text: 'Kill Chain Phase', datafield: 'kill_phase', width: 180, cellsalign: 'right', cellsformat: 'c2',renderer:columnrenderer,cellsrenderer:cellsrenderer },
-
-                ]
-            });
-    }
-
-    function downloadFile(filename, text) {
-        console.log("Trying to download the file");
-        var element = document.createElement('a');
-        element.setAttribute('href', 'data:text/plain;charset=utf-8,' + encodeURIComponent(text));
-        element.setAttribute('download', filename);
-        element.style.display = 'none';
-        document.body.appendChild(element);
-        element.click();
-        document.body.removeChild(element);
-    }
-
-    function threatDistributionGrid(name,dataLoad){
-        console.log("Threat Distribution Table");
-        console.log(dataLoad);
+function loadDataGridWithName(dataLoad,name){
+        console.log("(...)(..)(..)(..)"+dataLoad);
         var source =
-            {
-                localdata: dataLoad,
-                datatype: "array",
-                datafields: [{ name: 'threat_action'},
-                  { name: 'risk',type:'float'},
+        {
+            localdata: dataLoad,
+            datatype: "array",
+            datafields: [{ name: 'sc_control'},
+              { name: 'id',type:'float'},
+              { name: 'sec_func',},
+              { name: 'en_level',},
+              { name: 'kill_phase', },
 
-           ] };
+       ] };
 
-            var dataAdapter = new $.jqx.dataAdapter(source, {
-                loadComplete: function (data) { },
-                loadError: function (xhr, status, error) { }
-            });
-            $("#"+name).jqxGrid(
-            {
-                source: dataAdapter,
-                autowidth: true,
-                autoheight:true,
-                pageable:true,
-                sortable: true,
-                theme:'darkblue',
-                columns: [
-                  { text: 'Threat Action Distribiution (%)', datafield: 'threat_action', width:250,renderer:columnrenderer,cellsrenderer:cellsrenderer},
-                  { text: 'Risk Distribution (%)', datafield:'risk', width:250,renderer:columnrenderer,cellsrenderer:cellsrenderer},
+        var dataAdapter = new $.jqx.dataAdapter(source, {
+            loadComplete: function (data) { },
+            loadError: function (xhr, status, error) { }
+        });
+        $("#"+name).jqxGrid(
+        {
+            source: dataAdapter,
+            autowidth: true,
+            autoheight:true,
+            pageable:true,
+            sortable: true,
+            columns: [
+              { text: 'Security Control', datafield: 'sc_control', width: 450,renderer:columnrenderer,cellsrenderer:cellsrenderer},
+              { text: 'Id', datafield:'id', width:  70,renderer:columnrenderer,cellsrenderer:cellsrenderer},
+              { text: 'Security Function', datafield: 'sec_func', width: 180,renderer:columnrenderer,cellsrenderer:cellsrenderer },
+              { text: 'Enforcement Level', datafield: 'en_level', width: 180, cellsalign: 'right',renderer:columnrenderer,cellsrenderer:cellsrenderer },
+              { text: 'Kill Chain Phase', datafield: 'kill_phase', width: 180, cellsalign: 'right', cellsformat: 'c2',renderer:columnrenderer,cellsrenderer:cellsrenderer },
 
-                ]
-            });
-            $("#"+name).jqxGrid({ pagesize:20});
+            ]
+        });
+}
+
+function downloadFile(filename, text) {
+    console.log("Trying to download the file");
+    var element = document.createElement('a');
+    element.setAttribute('href', 'data:text/plain;charset=utf-8,' + encodeURIComponent(text));
+    element.setAttribute('download', filename);
+    element.style.display = 'none';
+    document.body.appendChild(element);
+    element.click();
+    document.body.removeChild(element);
+}
+
+function threatDistributionGrid(name,dataLoad){
+    console.log("Threat Distribution Table");
+    console.log(dataLoad);
+    var source =
+        {
+            localdata: dataLoad,
+            datatype: "array",
+            datafields: [{ name: 'threat_action'},
+              { name: 'risk',type:'float'},
+
+       ] };
+
+        var dataAdapter = new $.jqx.dataAdapter(source, {
+            loadComplete: function (data) { },
+            loadError: function (xhr, status, error) { }
+        });
+        $("#"+name).jqxGrid(
+        {
+            source: dataAdapter,
+            autowidth: true,
+            autoheight:true,
+            pageable:true,
+            sortable: true,
+            theme:'darkblue',
+            columns: [
+              { text: 'Threat Action Distribiution (%)', datafield: 'threat_action', width:250,renderer:columnrenderer,cellsrenderer:cellsrenderer},
+              { text: 'Risk Distribution (%)', datafield:'risk', width:250,renderer:columnrenderer,cellsrenderer:cellsrenderer},
+
+            ]
+        });
+        $("#"+name).jqxGrid({ pagesize:20});
+}
+
+function generateInputFiles(asset_list){
+    var writeFile = "";
+    asset_name_tag = '<asset>';
+    asset_name_tag_end = '</asset>';
+    var validKeyList = ['asset_name','availability','confidentiality','data_source','frequency','integrity','threat_list'];
+    for (var i = 0;i<asset_list.length;i++){
+        writeFile += asset_name_tag + '\n';
+        for (var key in asset_list[i]){
+            if (!validKeyList.includes(key)) continue;
+            if(key=='threat_list'){
+                writeFile += '<threat_list>\n';
+                for (var j=0;j<asset_list[i]['threat_list'].length;j++){
+                    for (var keyThreat in asset_list[i]['threat_list'][j]){
+                        writeFile += keyThreat + ":" +asset_list[i]['threat_list'][j][keyThreat]+",";
+                    }
+                    writeFile += '\n';
+                }
+                writeFile += '</threat_list>\n';
+            }
+            else{
+                 writeFile += key + ":"+asset_list[i][key]+"\n";
+            }
+        }
+        writeFile += asset_name_tag_end + '\n\n';
     }
+    console.log(writeFile);
+    downloadFile("InputFile.txt", writeFile);
+
+}
+
+
 
 
 
