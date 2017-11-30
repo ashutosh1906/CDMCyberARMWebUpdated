962ea609d0983d7a0e34ed34cb4bd3d8dd3c8b14
Dutta <adutta6@uncc.edu>
2017-11-30 15:30:35 -0500

    Nov Last First Commit

--- CDMBuilder/CyberARMDeployed/CyberARMEngineDistribution.py
+++ CDMBuilder/CyberARMDeployed/CyberARMEngineDistribution.py
@@ -18,25 +18,7 @@ def select_security_controls(security_control_list,threat_action_list,threat_act
     selected_security_controls = []
     threat_id_for_all_assets = []
     threat_action_id_list_for_all_assets = []
-    ################################################################################## End Global Variables ################################################################
-    # for i in range(len(risk_threat_action)):
-    #     for threat_action_specific_asset_list in risk_threat_action[i]:
-    #         threat_action_name_list_specific_asset = []
-    #         for threat_action_specific_asset in threat_action_specific_asset_list.keys():
-    #             if threat_action_specific_asset_list[threat_action_specific_asset] > ProjectConfigFile.THREAT_PRIORITIZATION_THRESHOLD:
-    #                 ta_index = 0
-    #                 for ta in threat_action_name_list_specific_asset:
-    #                     if ta[1] < threat_action_specific_asset_list[threat_action_specific_asset]:
-    #                         break
-    #                     ta_index += 1
-    #                 threat_action_name_list_specific_asset.insert(ta_index,[threat_action_name_to_id[threat_action_specific_asset],threat_action_specific_asset_list[threat_action_specific_asset]])
-    #         threat_action_name_list.append(threat_action_name_list_specific_asset)
-    # Utitilities.printThreatActionNameListIter(threat_action_name_list)
-    # ######################################################################### Prune The Threat Action Name List Here ####################
-    # for threat_action_list_specific_asset_index in range(len(threat_action_name_list)):
-    #     threat_action_name_list[threat_action_list_specific_asset_index] = threat_action_name_list[threat_action_list_specific_asset_index][0:ProjectConfigFile.CHOSEN_NUMBER_THREAT_ACTION]
-    # ######################################################################### End of Pruning The Threat Action Name List Here ###################
-    # # Utitilities.printThreatActionNameListIter(threat_action_name_list)
+
     for i in range(len(asset_enterprise_list)):
         for j in range(len(asset_enterprise_list[i])):
             threat_action_name_list.append([])
